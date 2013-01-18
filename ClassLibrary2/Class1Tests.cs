5696518f0e3e269c2ac35c764a82af39488bece9
Jason More <Jason.More@gmail.com>
2013-01-18 08:42:24 -0600

    init

---
+++ ClassLibrary2/Class1Tests.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    [TestFixture]
+    public class Class1Tests
+    {
+        [Test]
+        public void TheTest()
+        {
+            var class1 = new Class1();
+            Assert.AreEqual("Foo Bar", class1.DoStuff());
+
+        }
+    }
+}
